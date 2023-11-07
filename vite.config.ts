07ecf4ea914aca9ee4a843e971baf8942813dfc7
Hamish Johnson <hamish@magpiedigital.io>
2023-11-07 18:46:30 +0700

    get flowise embed working with vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,18 @@
-import { defineConfig } from 'vite'
-import solid from 'vite-plugin-solid'
+import path from "path";
+import { defineConfig } from "vite";
+import solid from "vite-plugin-solid";
 
 export default defineConfig({
   plugins: [solid()],
-})
+  build: {
+    lib: {
+      name: "Chatbot",
+      entry: path.resolve(__dirname, "src/web.ts"),
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
