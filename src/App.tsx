44de00398d2cd15c7977b5e09d1e1aea594ba6b9
LucasKnudsen <laksteelhouse@gmail.com>
2023-11-07 13:51:11 +0100

    Adding dev bot id

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,12 @@ import './dev.css'
 import { Full } from './features/full'
 
 function App() {
-  return <Full chatflowid='asd' />
+  return (
+    <Full
+      chatflowid='ca719387-f573-4989-aea0-21dc07d5ca73'
+      apiHost='https://flowise.testnet.concordium.com'
+    />
+  )
 }
 
 export default App
