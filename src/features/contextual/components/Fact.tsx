8cf70e8a746dc9b3386427cf23b6c0e64c41b504
LucasKnudsen <laksteelhouse@gmail.com>
2023-11-15 17:12:31 +0100

    Adds basic Link

--- src/features/contextual/components/Fact.tsx
+++ src/features/contextual/components/Fact.tsx
@@ -7,8 +7,8 @@ type Props = {
 export const Fact = ({ element }: Props) => {
   return (
     <div class='flex flex-row items-center justify-between py-2 border-b border-gray-200 '>
-      <p class='pr-12 text-sm font-medium text-gray-500'>{element.header}:</p>
-      <p class='text-sm font-medium text-gray-900 text-right'>
+      <p class='pr-8 text-sm font-medium text-gray-500'>{element.header}:</p>
+      <p class='text-sm font-medium text-gray-900 text-right break-words'>
         {Array.isArray(element.value) ? element.value.join(', ') : element.value}
       </p>
     </div>
--- src/features/contextual/components/Link.tsx
+++ src/features/contextual/components/Link.tsx
@@ -5,8 +5,17 @@ type Props = {
 }
 
 export const Link = ({ element }: Props) => {
+  if (Array.isArray(element.value)) return null
+
   // TODO: Add onClick to open link in new tab
   // TODO: Add onHover to show description and link indication
 
-  return null
+  return (
+    <div class='flex flex-row items-center justify-between py-2 border-b border-gray-200 '>
+      <p class='pr-8 text-sm font-medium text-gray-500 whitespace-nowrap'>Read more:</p>
+      <a class='text-sm break-words text-right' href={element.value} target='_blank'>
+        {element.value}
+      </a>
+    </div>
+  )
 }
