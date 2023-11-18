ec57c23aa579e5389c5ea3ebbc918408397bda36
LucasKnudsen <laksteelhouse@gmail.com>
2023-11-18 12:50:12 +0100

    Adds amplify hosting. Build errors

--- amplify/backend/function/flowiseMiddleware/lib/index.ts
+++ amplify/backend/function/flowiseMiddleware/lib/index.ts
@@ -1,18 +1,29 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import axios from 'axios'
 
+// const chatflowid = 'ca719387-f573-4989-aea0-21dc07d5ca73'
+// const apiHost = 'https://flowise.testnet.concordium.com'
+const TEST_API_KEY = 'Bearer Z6tQxMs34lQs1kcpOO7bB8bbMrUY9cDo52kjopo/MjM='
+
 export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   console.log(`EVENT: ${JSON.stringify(event)}`)
 
-  const chatflowid = 'ca719387-f573-4989-aea0-21dc07d5ca73'
-  const apiHost = 'https://flowise.testnet.concordium.com'
+  const { chatflowid, apiHost } = event.body
+    ? JSON.parse(event.body)
+    : { chatflowid: '', apiHost: '' }
 
   const endpoint = `${apiHost}/api/v1/prediction/${chatflowid}`
 
   try {
-    const result = await axios.post(endpoint, JSON.parse(event.body))
+    const result = await axios.post(endpoint, JSON.parse(event.body), {
+      headers: {
+        Authorization: TEST_API_KEY,
+      },
+    })
 
     const answer = result.data
+
+    console.log(`ANSWER: `, answer)
 
     return {
       statusCode: 200,
@@ -23,8 +34,10 @@ export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayPr
       body: JSON.stringify(answer),
     }
   } catch (error) {
+    console.log(`ERROR: `, JSON.stringify(error))
+
     return {
-      statusCode: 500,
+      statusCode: error.response?.status || 500,
       headers: {
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Headers': '*',
