9c949db3df1d59bf423dcbea603e6090b3de922c
LucasKnudsen <laksteelhouse@gmail.com>
2023-11-22 10:35:48 +0100

    Redoing amplify hosting

--- vite.config.ts
+++ vite.config.ts
@@ -10,12 +10,12 @@ export default defineConfig({
       entry: path.resolve(__dirname, 'src/web.ts'),
     },
   },
-  define: {
-    // Fixes the Amplify global issue
-    global: {},
-    // Fixes Amplify build issue 🤔 https://stackoverflow.com/questions/75925195/how-to-fix-vite-build-syntax-error-unexpected-token-in-third-party-dependenc
-    _global: {},
-  },
+  // define: {
+  //   // Fixes the Amplify global issue
+  //   global: {},
+  //   // Fixes Amplify build issue 🤔 https://stackoverflow.com/questions/75925195/how-to-fix-vite-build-syntax-error-unexpected-token-in-third-party-dependenc
+  //   _global: {},
+  // },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
