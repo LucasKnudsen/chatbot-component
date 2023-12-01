fa8b068f426aedf0edd4130edb2bb38b18409044
LucasKnudsen <laksteelhouse@gmail.com>
2023-12-01 16:01:51 +0100

    Adds pulse animation to facts while translating

--- src/features/contextual/components/Fact.tsx
+++ src/features/contextual/components/Fact.tsx
@@ -1,4 +1,5 @@
 import { useTheme } from '@/features/theme/hooks'
+import { Show } from 'solid-js'
 import { ContextualElement } from '..'
 
 type Props = {
@@ -11,17 +12,19 @@ export const Fact = (props: Props) => {
 
   return (
     <div
-      class={'p-2 text-sm ' + props.class}
+      class={'p-2 text-sm ' + props.class + ' ' + (!props.fact.header ? 'animate-pulse' : '')}
       style={{
         background: theme().surfaceBackground2,
         'border-left': `3px solid ${theme().primaryColor}`,
         color: theme().textColor,
       }}
     >
-      <div class='font-semibold'>{props.fact.header}</div>
-      <div class='font-light'>
-        {Array.isArray(props.fact.value) ? props.fact.value.join(', ') : props.fact.value}
-      </div>
+      <Show when={props.fact.header} fallback={<div class='h-14' />}>
+        <div class='font-semibold'>{props.fact.header}</div>
+        <div class='font-light'>
+          {Array.isArray(props.fact.value) ? props.fact.value.join(', ') : props.fact.value}
+        </div>
+      </Show>
     </div>
   )
 }
