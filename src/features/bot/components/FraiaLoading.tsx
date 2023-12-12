1b9123ba9d671cec02822dab62a7d99767fe19df
LucasKnudsen <laksteelhouse@gmail.com>
2023-12-12 13:41:45 +0100

    Fraia primary color fix

--- src/features/bot/components/FraiaLoading.tsx
+++ src/features/bot/components/FraiaLoading.tsx
@@ -1,13 +1,16 @@
 import { TypingBubble } from '@/components'
+import { useTheme } from '@/features/theme'
 
 export const FraiaLoading = () => {
+  const { theme } = useTheme()
+
   return (
     <div class='w-screen h-screen flex flex-col justify-center items-center  animate-fade-in'>
       <h1 class='text-5xl mb-4'>
         FR
         <span
           style={{
-            color: '#3B81F6',
+            color: theme().primaryColor,
           }}
         >
           AI
