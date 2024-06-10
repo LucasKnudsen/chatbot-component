4b668fe5e9d9198a34fe9633fc26de8d34b8df42
Edward Ha <quocvieth08@gmail.com>
2024-06-10 21:05:40 +0700

    Add PR template

--- src/features/bot/components/FraiaLoading.tsx
+++ src/features/bot/components/FraiaLoading.tsx
@@ -1,14 +1,12 @@
-import { TypingBubble, ProgressBar } from '@/components'
+import { ProgressBar } from '@/components'
 import { LogoIcon } from '@/components/icons/LogoIcon'
 import { useTheme } from '@/features/theme'
-import { configStore } from '@/features/portal-init'
 
 type Props = {
   isLoading?: boolean;
 }
 export const FraiaLoading = (props: Props) => {
   const { theme } = useTheme()
-  const { mode } = configStore
 
   return (
     <div class='h-full w-screen flex flex-col grow justify-center animate-fade-in gap-4 p-10 lg:p-24'>
@@ -16,24 +14,10 @@ export const FraiaLoading = (props: Props) => {
         <div class='inline-block mb-4'>
           <LogoIcon color={theme().primaryColor} />
         </div>
-        {mode === 'oneClick' ? (
           <div class='leading-[17px]'>
             <ProgressBar isLoading={props.isLoading}  />
           </div>
-        ) : (
-          <>
-          <h4
-            class='text-base leading-[17px] font-light'
-            style={{
-              color: theme().textSecondary,
-            }}
-          >
-            The most personalized AI catchphrase
-          </h4>
-          </>
-        )}
       </div>
-      {mode !== 'oneClick' && <TypingBubble />}
     </div>
   )
 }
