d35a05a235328765e2488b625152fe0d6eb6a289
Edward Ha <56016006+QuocVietHa08@users.noreply.github.com>
2024-06-25 14:45:32 +0700

    Feature handle transcription (#13)
    Co-authored-by: Edward Ha <quocvieth08@gmail.com>

--- src/features/oneClick/BotOneClick.tsx
+++ src/features/oneClick/BotOneClick.tsx
@@ -10,56 +10,125 @@ import {
 import { Conversation } from './Conversation'
 import { oneClickActions, oneClickStore } from './store/oneClickStore'
 import { BotStatus, ChatMessage } from './types'
+import { buildBodyForLLM, createAudioRecorder } from '../avatar'
+import { transcribeAudio } from '../knowledge-base'
+import { NEXT_API_ENDPOINTS } from '@/constants/api'
+import { queryLLM } from '@/features/messages'
 
 export const BotOneClick = () => {
   const [isStart, setIsStart] = createSignal<boolean>(false)
   const [conversation, setConversation] = createSignal<ChatMessage[]>([])
 
+  let audioRef: any;
+
   const [TopContainerParent] = createAutoAnimate()
+
+  const audioRecorder = createAudioRecorder({
+    // onStop(audioBlob) {
+    //   handleVoiceToVoice(audioBlob)
+    // }, 
+  })
 
   const handleButtonRecord = () => {
     if (!isStart()) {
       setIsStart(true)
       oneClickActions.setStatus(BotStatus.LISTENING)
+      audioRecorder.startRecording();
       return
     }
+
     if (isStart() && oneClickStore.botStatus === BotStatus.LISTENING) {
+      audioRecorder.stopRecording();
       oneClickActions.setStatus(BotStatus.THINKING)
       return
     }
     if (isStart() && oneClickStore.botStatus === BotStatus.ANSWERING) {
       oneClickActions.setStatus(BotStatus.IDLE)
+      audioRef.pause();
       setIsStart(false)
       return
     }
   }
 
-  const handleSwitchBotStatus = () => {
-    switch (oneClickStore.botStatus) {
-      case BotStatus.THINKING:
-        const userConversation: ChatMessage = {
-          message: 'Tell me about the Author Neil Gaiman..',
-          role: 'user',
-        }
-        setConversation((prev) => [...prev, userConversation])
-        setTimeout(() => {
-          oneClickActions.setStatus(BotStatus.ANSWERING)
-        }, 3000)
-        break
+  const handleTranscription = async (audioBlob: Blob) => {
+    if (!audioBlob) {
+      throw new Error('No audio blob found')
+    }
 
-      case BotStatus.ANSWERING:
-        setTimeout(() => {
-          oneClickActions.setStatus(BotStatus.IDLE)
-          setIsStart(false)
-          const botConversation: ChatMessage = {
-            message: 'Lorem ipsum dolor sit amet consectetur. Enim integer iaculis dictum metus',
-            role: 'assistant',
-          }
-          setConversation((prev) => [...prev, botConversation])
-        }, 4000)
-        break
-      default:
-        break
+    let transcribedText = ''
+    const audioFile = new File([audioBlob], `audio.${audioBlob.type.split('/')[1]}`, {
+      type: audioBlob.type,
+    })
+
+    const formData = new FormData()
+    formData.append('file', audioFile)
+    formData.append('llmArguments', JSON.stringify(buildBodyForLLM()))
+
+    if (audioBlob.type.includes('mp4')) {
+      const transcriptionResponse = await transcribeAudio(audioFile, {
+        diarization_toggle: false,
+      })
+
+      transcribedText = transcriptionResponse.transcription[0].text
+    } else {
+      const transcriptionResponse = await fetch(NEXT_API_ENDPOINTS.speechToText, {
+        method: 'POST',
+        body: formData,
+      })
+
+      transcribedText = (await transcriptionResponse.json()).data.transcription
+    }
+
+    return transcribedText
+  }
+
+  const handleQuery = async (message: string) => {
+    const textForTTS = await queryLLM(message)
+
+    return textForTTS
+  }
+
+  const handleVoiceReply = async (text: string) => {
+    const audioStream = await fetch(NEXT_API_ENDPOINTS.textToSpeech, {
+      method: 'POST',
+
+      body: JSON.stringify({
+        text,
+        voiceId: oneClickStore.activeChannel?.overrideConfig?.elevenLabsVoiceId || '',
+      }),
+    })
+    if (!audioStream.body) return
+
+    const base64String = (await audioStream.json()).data
+
+    const audioSrc = `data:audio/mp3;base64,${base64String}`
+    audioRef.src = audioSrc
+
+    const onAudioEnded = () => {
+      audioRef.removeEventListener('ended', onAudioEnded)
+      oneClickActions.setStatus(BotStatus.IDLE)
+      setIsStart(false);
+    }
+
+    audioRef.addEventListener('ended', onAudioEnded)
+
+    audioRef.play()
+  }
+
+  const handleVoiceToVoice = async (audioBlob: Blob) => {
+    try {
+      oneClickActions.setStatus(BotStatus.THINKING);
+      const transcribedText = await handleTranscription(audioBlob)
+      // Queries the LLM to get the text answer
+      const textForTTS = await handleQuery(transcribedText)
+      oneClickActions.setStatus(BotStatus.ANSWERING)
+      // Gets the audio from the TTS
+      await handleVoiceReply(textForTTS || '')
+    } catch (error) {
+      console.error('Error in handleVoiceToVoice', error)
+    } finally {
+      console.timeEnd('Bot answer');
+
     }
   }
 
@@ -70,7 +139,6 @@ export const BotOneClick = () => {
       }, 2000)
       return
     }
-    handleSwitchBotStatus()
   })
 
   return (
@@ -115,6 +183,8 @@ export const BotOneClick = () => {
       </div>
 
       <Conversation messages={conversation()} />
+
+      <audio ref={audioRef} crossOrigin='anonymous' src={audioRef.src} class='hidden'></audio>
 
       {/* Text Input  */}
       <InputOneClick />
