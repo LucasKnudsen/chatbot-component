802b9a680b74f0d63b1a8309cc43a64b97a7adae
LucasKnudsen <laksteelhouse@gmail.com>
2024-11-15 14:59:14 +0100

    final fix

--- src/features/oneClick/components/NavMenu.tsx
+++ src/features/oneClick/components/NavMenu.tsx
@@ -10,7 +10,7 @@ import { initiateConversation } from '../services'
 import { oneClickActions, oneClickStore } from '../store/oneClickStore'
 
 const MenuItem = ({ Icon, text, onClick }: { text: string; Icon: any; onClick?: () => void }) => {
-  const handleOnClick = (e: any) => {
+  const handleOnClick = () => {
     onClick?.()
   }
 
