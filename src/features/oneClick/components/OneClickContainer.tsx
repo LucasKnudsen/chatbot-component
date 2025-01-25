6f231b19edbbd02789e6733ff9874da27d1bf396
LucasKnudsen <laksteelhouse@gmail.com>
2025-01-25 11:51:02 +0700

    fixing a bug that caused text not to be selectable on some sites

--- src/features/oneClick/components/OneClickContainer.tsx
+++ src/features/oneClick/components/OneClickContainer.tsx
@@ -14,7 +14,7 @@ export const OneClickContainer = (props: { children: JSX.Element }) => {
     <div
       class={`fixed border-[var(--borderColor)] animate-fade-in top-0 left-0 w-screen md:rounded-xl lg:w-[400px] max-w-full max-h-full xl:w-[450px] lg:shadow-xl xl:h-[80vh] lg:max-h-[700px] xl:max-h-[700px] xxl:h-[85vh] lg:top-auto lg:left-auto lg:right-[20px] lg:bottom-[20px] m-0 min-h-[-webkit-fill-available] lg:min-h-[200px] lg:border h-full ${
         configStore.isBotOpened ? 'opacity-1' : 'opacity-0 pointer-events-none'
-      }`}
+      } select-text`}
       style={{
         transition: 'transform 350ms cubic-bezier(0, 1.2, 1, 1), opacity 250ms ease-out',
         'transform-origin': 'bottom right',
@@ -26,14 +26,14 @@ export const OneClickContainer = (props: { children: JSX.Element }) => {
           heyGenStore.isExpandAvatar || configStore.isInFullScreenMode || device() !== 'desktop'
             ? '100vw'
             : configStore.styleConfig?.containerWidth
-            ? `${configStore.styleConfig.containerWidth}`
-            : '',
+              ? `${configStore.styleConfig.containerWidth}`
+              : '',
         height:
           heyGenStore.isExpandAvatar || configStore.isInFullScreenMode || device() !== 'desktop'
             ? '100vh'
             : configStore.styleConfig?.containerHeight
-            ? `${configStore.styleConfig.containerHeight}`
-            : '',
+              ? `${configStore.styleConfig.containerHeight}`
+              : '',
         bottom: heyGenStore.isExpandAvatar || configStore.isInFullScreenMode ? 0 : '',
         right: heyGenStore.isExpandAvatar || configStore.isInFullScreenMode ? 0 : '',
         'border-radius': heyGenStore.isExpandAvatar || configStore.isInFullScreenMode ? 0 : '',
